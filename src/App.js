cc8938c1fcd8e010c35d4ab4a560e78c7b2bd79d
Achyut Timsina <actimsina@gmail.com>
2023-01-12 09:13:40 +0545

    half stack notes app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import { useState } from "react";
-import Feedback from "./components/Feedback";
 
-function App({ name, age }) {
-  const [counter, setCounter] = useState(0)
+function App(props) {
 
-  // setTimeout(() => setCounter(counter + 1), 1000)
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
-  console.log(`Rendering ${counter} ...`)
 
-  const handlePlus = () => setCounter(counter + 1)
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(n => n.important === true)
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const handleAdd = (event) => {
+    event.preventDefault()
+    const note = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    }
+
+    if (newNote !== '') setNotes(notes.concat(note))
+    setNewNote('')
+  }
 
 
   return (
     <>
-      <h2>Hello {name}, You're {age} years old.</h2>
-      <h2>{counter}</h2>
-      <button onClick={handlePlus}>Plus</button>
+      <h2>Notes</h2>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? 'important' : 'all'}
+      </button>
+      <ul>
+        {notesToShow.map(note =>
+          <li key={note.id}>
+            <p>{note.content}</p>
+            <p>{note.date}</p>
+          </li>
+        )}
+      </ul>
 
-      <Feedback />
+      <form>
+        <input value={newNote} onChange={handleInputChange} />
+        <button onClick={handleAdd}>add</button>
+      </form>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'
+
+const notes = [
+  {
+    "id": 1,
+    "content": "HTML is easy",
+    "date": "2022-1-17T17:30:31.098Z",
+    "important": true
+  },
+  {
+    "id": 2,
+    "content": "Browser can execute only JavaScript",
+    "date": "2022-1-17T18:39:34.091Z",
+    "important": false
+  },
+  {
+    "id": 3,
+    "content": "GET and POST are the most important methods of HTTP protocol",
+    "date": "2022-1-17T19:20:14.298Z",
+    "important": false
+  }
+]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App name={'Ram'} age={25} />
+  <App notes={notes} />
 );
 
